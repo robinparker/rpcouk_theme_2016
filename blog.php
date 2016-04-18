38ca74983333e03355194670fd6ed19dd11ed314
Robin Parker <robinkristianparker@gmail.com>
2016-04-18 19:21:44 +0100

    Blog template updates

--- blog.php
+++ blog.php
@@ -22,14 +22,24 @@ get_header(); ?>
 		$args = array( 'post_type' => 'post', 'posts_per_page' => 10 );
 		$loop = new WP_Query( $args );
 		while ( $loop->have_posts() ) : $loop->the_post();
-			echo '<div class="row">';
+			echo '<div class="row post-summary">';
 			echo '<a href="' .get_permalink() .'">';
-		  // the_title();
 			?>
-			<!-- <img src="<?php the_field('main_image'); ?>" /> -->
+			<div class="post-summary_img">
+				<?php
+				the_post_thumbnail();
+				?>
+			</div>
+			<div class="post-summary_text">
+				<h2><?php the_title(); ?></h2>
+			</div>
+			<article>
+
+
+			</article>
 			<?php
-			the_post_thumbnail('large');
-			the_excerpt();
+			// the_post_thumbnail('large');
+			// the_excerpt();
 		  // echo '<div class="entry-content">';
 		  // the_content();
 			// the_permalink();
