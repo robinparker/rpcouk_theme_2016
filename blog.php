393dee6a2e141b089884faff95541f3779af8afd
Robin Parker <robinkristianparker@gmail.com>
2016-04-18 20:13:42 +0100

    blog template updates

--- blog.php
+++ blog.php
@@ -17,21 +17,26 @@ get_header(); ?>
 
 		<main id="main" class="site-main" role="main">
 
+			<header class="section-heading"><h2>Blog</h2></header>
 
 		<?php
 		$args = array( 'post_type' => 'post', 'posts_per_page' => 10 );
 		$loop = new WP_Query( $args );
 		while ( $loop->have_posts() ) : $loop->the_post();
 			echo '<div class="row post-summary">';
-			echo '<a href="' .get_permalink() .'">';
+
 			?>
 			<div class="post-summary_img">
 				<?php
+				echo '<a href="' .get_permalink() .'">';
 				the_post_thumbnail();
+				echo '</a>';
 				?>
 			</div>
 			<div class="post-summary_text">
-				<h2><?php the_title(); ?></h2>
+				<h3><?php the_title(); ?></h3>
+				<span><?php the_date(); ?></span>
+				<p><?php the_excerpt(); ?></p>
 			</div>
 			<article>
 
@@ -44,7 +49,7 @@ get_header(); ?>
 		  // the_content();
 			// the_permalink();
 		  // echo '</div>';
-			echo '</a>';
+
 			echo '</div>';
 		endwhile;
 		?>
--- functions.php
+++ functions.php
@@ -47,6 +47,35 @@ function rpcouk_setup() {
 		'primary' => esc_html__( 'Primary', 'rpcouk' ),
 	) );
 
+
+	/**
+	 * Filter the except length to 20 characters.
+	 *
+	 * @param int $length Excerpt length.
+	 * @return int (Maybe) modified excerpt length.
+	 */
+	function wpdocs_custom_excerpt_length( $length ) {
+	    return 30;
+	}
+	add_filter( 'excerpt_length', 'wpdocs_custom_excerpt_length', 999 );
+
+
+
+	/**
+	 * Filter the "read more" excerpt string link to the post.
+	 *
+	 * @param string $more "Read more" excerpt string.
+	 * @return string (Maybe) modified "read more" excerpt string.
+	 */
+	function wpdocs_excerpt_more( $more ) {
+	    return sprintf( '<a class="read-more" href="%1$s">%2$s</a>',
+	        get_permalink( get_the_ID() ),
+	        __( 'Read More', 'textdomain' )
+	    );
+	}
+	add_filter( 'excerpt_more', 'wpdocs_excerpt_more' );
+
+
 	/*
 	 * Switch default core markup for search form, comment form, and comments
 	 * to output valid HTML5.
